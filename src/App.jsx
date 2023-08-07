e00f4da8c141b047071378a071ebf8865d7a0ddf
atunbi <damilareatunbi@gmail.com>
2023-08-07 15:32:32 +0100

    start to finished project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ProductList from "../src/components/product/ProductList";
+
+function App() {
+  return (
+    <div>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/product/ProductList.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import Categories from "../categories/Categories";
+import Search from "../search/Search";
+import Product from "./Product";
+import "./ProductList.css";
+import { products as productData } from "../../products-data";
+
+const allCategories = [ 
+  "all",
+  ...new Set(productData.map((product) => product.category)
+  )
+];
+
+const ProductList = () => {
+  const [products, setProducts] = useState(productData);
+  const [search, setSearch] = useState("");
+  const [categories, setCategories] = useState(allCategories);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filterProduct = (category) => {
+    if (category === "all") {
+      setProducts(productData);
+      return;
+    }
+    const newProducts = productData.filter((product) => product.category === category);
+    setProducts(newProducts);
+  };
+
+  useEffect(() => {
+    console.log(allCategories);
+    setFilteredProducts(products.filter((product) => product.title.toLocaleLowerCase().includes(search.toLocaleLowerCase())));
+  }, [search, products]);
+
+  return (
+    <>
+      <div className="header">
+        <header className="container">
+          <h1 className="--color-white --text-center">
+            <span className="--color-danger">Product</span> Filter
+          </h1>
+          <div className="--flex-between --flex-dir-column --py">
+            <Search inputValue={search} onInputChange={handleSearch} />
+            <Categories categories={categories} filterItems={filterProduct}/>
+          </div>
+        </header>
+      </div>
+      <div className="product-container">
+        <div className="products containe --grid-25 --py2">
+          {filteredProducts.length === 0 ? (
+            <h3 className="--text-center">This Product does not Exist</h3>
+          ) : (
+            filteredProducts.map((product) => {
+              const { id, price, img, title } = product;
+              return (
+                <div key={id}>
+                  <Product title={title} img={img} price={price} />
+                </div>
+              );
+            })
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductList;
